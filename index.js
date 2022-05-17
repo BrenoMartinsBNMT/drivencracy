002a3d027e730ba37aa42ad9b36a6f50e56c7e69
breno <brenoamrtinscunha@gmail.com>
2022-05-17 17:43:08 -0300

    heroku deploy

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
-import express, { json } from "express";
+import express, { json, Router } from "express";
 import dotenv from "dotenv";
 import dayjs from "dayjs";
 import cors from "cors";
 
 import db from "./db.ja";
 
+const router = Router();
 const app = express();
 app.use(json());
 app.use(cors());
 dotenv.config();
+
+router.get("/", (req, res) => {
+  res.send("chegou no servidor");
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
