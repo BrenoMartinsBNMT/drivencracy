5857627c23631bde991e0431f7240943fd94bcc6
breno <brenoamrtinscunha@gmail.com>
2022-05-17 17:39:31 -0300

    heroku deploy

--- db.js
+++ db.js
@@ -5,4 +5,9 @@ dotenv.config();
 
 try {
   const mongoClient = new MongoClient(process.env.MONGO_URL);
-} catch {}
+  await mongoClient.connect();
+  let db = mongoClient.db(process.env.DATABASE);
+} catch (e) {
+  console.log("error ao se conectar ao banco", e);
+}
+export default db;
--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+import express, { json } from "express";
+import dotenv from "dotenv";
+import dayjs from "dayjs";
+import cors from "cors";
 
+import db from "./db.ja";
+
+const app = express();
+app.use(json());
+app.use(cors());
+dotenv.config();
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`O servidor está em pé ${port}`);
+});
